3507a27c7459d27c434e47a5e3e195ae5eabdbad
Norman <el.norman01@gmail.com>
2016-01-06 00:01:05 -0400

    Utilidad de Imagen
    -Se crea la clase PictureBinary que permite crear el binario de una
    imagen jpg y crear una imagen a partir del binario de la base de datos.

---
+++ ArrobaGym/Utils/PictureBinary.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace ArrobaGym.Utils
+{
+    public class PictureBinary
+    {
+        private PictureBinary() {
+        }
+
+        public static System.Data.Linq.Binary GetBinary(string filename)
+        {
+            Image img = Image.FromFile(filename);
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.GetBuffer();
+        }
+
+        public static Image GetImage(System.Data.Linq.Binary binary)
+        {
+            MemoryStream ms = new MemoryStream(binary.ToArray());
+            Image img = Image.FromStream(ms);
+            return img;
+        }
+    }
+}
