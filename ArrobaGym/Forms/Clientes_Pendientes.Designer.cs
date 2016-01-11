27f83d55f1c0cfc2e1989e223712dfeeeca455fa
Norman <el.norman01@gmail.com>
2016-01-10 23:06:10 -0400

    Cliente Pendiente
    -El formulario de cliente pendiente puede abir por boton el cliente
    seleccionado.

--- ArrobaGym/Forms/Clientes_Pendientes.Designer.cs
+++ ArrobaGym/Forms/Clientes_Pendientes.Designer.cs
@@ -35,11 +35,15 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(15, 34);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(420, 267);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridView1_RowStateChanged);
             // 
             // bntCobrar
             // 
--- ArrobaGym/Forms/Clientes_Pendientes.cs
+++ ArrobaGym/Forms/Clientes_Pendientes.cs
@@ -12,22 +12,43 @@ namespace ArrobaGym
 {
     public partial class Clientes_Pendientes : Form
     {
+        DAO.Repository<Models.Cliente> ClienteDAO = new DAO.Repository<Models.Cliente>();
+        Models.Cliente ClienteSeleccionado = null;
+
         public Clientes_Pendientes()
         {
             InitializeComponent();
-           
+            this.dataGridView1.ReadOnly = true;
+            bntCobrar.Enabled = false;
         }
 
         private void Clientes_Pendientes_Load(object sender, EventArgs e)
         {
-
+            this.dataGridView1.DataSource = ClienteDAO.FindAll(c => c.Status == "Pendiente")
+                .Select(c => new { c.Id, c.Nombre, c.Apellido, c.Status, c.Saldo }).ToArray();
+            this.dataGridView1.Columns["Id"].Visible = false;
         }
 
         private void bntCobrar_Click(object sender, EventArgs e)
         {
-            Clientes_Membresias_Cobrar cobro = new Clientes_Membresias_Cobrar();
-            cobro.Visible = true;
+            Clientes_Membresias_Cobrar cobro = new Clientes_Membresias_Cobrar(
+                this.ClienteSeleccionado
+                );
+            cobro.ShowDialog(this);
             this.Close();
+        }
+
+        private void dataGridView1_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            if (e.StateChanged != DataGridViewElementStates.Selected)
+            {
+                bntCobrar.Enabled = false;
+                return;
+            }
+            bntCobrar.Enabled = true;
+            int id = (int)e.Row.Cells["Id"].Value;
+            this.ClienteSeleccionado =
+                ClienteDAO.SelectSingle(c => c.Id == id);
         }
     }
 }
